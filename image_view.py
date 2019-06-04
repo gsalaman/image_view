18276877e48d4217ed1d43b81310e40a4840bc36
gsalaman <g.salaman@comcast.net>
2019-06-04 13:20:12 +0000

    Switching to simpler keyboard exit

--- image_view.py
+++ image_view.py
@@ -1,45 +1,10 @@
-###############################
-#  Imports for reading keyboard
-##############################
-import sys, os
-import termios, fcntl
-
-# used to slow down our main loop
-import time
-
-################################
-#  Initialize keyboard reading. 
-#  Save the old terminal configuration, and
-#  tweak the terminal so that it doesn't echo, and doesn't block.
-################################
-fd = sys.stdin.fileno()
-newattr = termios.tcgetattr(fd)
-
-oldterm = termios.tcgetattr(fd)
-oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-
-newattr[3] = newattr[3] & ~termios.ICANON
-newattr[3] = newattr[3] & ~termios.ECHO
-
-fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
-
-termios.tcsetattr(fd, termios.TCSANOW, newattr)
-
-##################################
-# Non-blocking character read function.
-#################################
-def getch_noblock():
-  try:
-    return sys.stdin.read()
-  except (IOError, TypeError) as e:
-    return None
+from time import sleep
 
 ###################################
 # Graphics imports, constants and structures
 ###################################
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image, ImageDraw
-
 
 # this is the size of ONE of our matrixes. 
 matrix_rows = 32
@@ -52,16 +17,13 @@ matrix_vertical = 1
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
 
-# the "draw size" for our ball in pixels 
-sprite_size = 5
-
 options = RGBMatrixOptions()
 options.rows = matrix_rows 
 options.cols = matrix_columns 
 options.chain_length = matrix_horizontal
 options.parallel = matrix_vertical 
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
-options.gpio_slowdown = 2
+#options.gpio_slowdown = 2
 
 matrix = RGBMatrix(options = options)
 
@@ -72,16 +34,10 @@ image = Image.open("dawson.jpg").convert('RGB')
 image = image.resize((32,32))
 matrix.SetImage(image, 0, 0)
 
-print "controls: q=quit"
-while True:
-  key = getch_noblock()
+try:
+  print("Press CTRL-C to stop")
+  while True:
+    sleep(100)
+except KeyboardInterrupt:
+  exit(0)
 
-  if key == 'q':
-     break    
-
-###################################
-# Reset the terminal on exit
-###################################
-termios.tcsetattr(fd, termios.TCSANOW, oldterm)
-
-fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
